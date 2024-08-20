a2da6001c5ce25be25ebc87ce3bc07cb234d41a6
Kamal Kumar <Kamal2k89@gmail.com>
2024-08-20 19:57:45 +0530

    docker-compose

---
+++ commands.sh
@@ -0,0 +1,40 @@
+# Update/Upgrade
+apt update && apt upgrade -y
+
+apt install -y docker.io docker-compose
+
+cat > docker-compose.yaml << EOF
+version: '3.1'
+
+services:
+
+  wordpress:
+    image: wordpress
+    restart: always
+    ports:
+      - 8080:80
+    environment:
+      WORDPRESS_DB_HOST: db
+      WORDPRESS_DB_USER: exampleuser
+      WORDPRESS_DB_PASSWORD: examplepass
+      WORDPRESS_DB_NAME: exampledb
+    volumes:
+      - wordpress:/var/www/html
+
+  db:
+    image: mysql:8.0
+    restart: always
+    environment:
+      MYSQL_DATABASE: exampledb
+      MYSQL_USER: exampleuser
+      MYSQL_PASSWORD: examplepass
+      MYSQL_RANDOM_ROOT_PASSWORD: '1'
+    volumes:
+      - db:/var/lib/mysql
+
+volumes:
+  wordpress:
+  db:
+EOF
+
+docker-compose -f docker-compose.yaml up
